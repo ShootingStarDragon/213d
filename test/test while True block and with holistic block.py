a67bcd28ab3eaabb6b6de65134f6a8d8f4fb8520
ShootingStarDragon <rppapamaths@gmail.com>
2023-03-13 11:45:52 -0700

    ok, time to rewrite so it's cleaner and with the knowledge of the 1st/2nd attempt

--- test/test while True block and with holistic block.py
+++ test/test while True block and with holistic block.py
@@ -24,42 +24,43 @@ while True:
         #yeah it went to 20 when I took down hoodie and gave a side~ish profile of my face
         #yeah it's not bluestacks, it's just a bit harder head-on 
         # while cap.isOpened():
+        while True:
+            # timef1 =  time.time()
+            # ret, frame = cap.read()
+            frame = np.full((10,10, 3), [255, 255, 255], dtype=np.uint8)
+            # timef2 =  time.time()
+            # print("how long to read frame?", timef2 - timef1)# first frame takes a while and subsequent frames are fast: 0.9233419895172119 -> 0.006009101867675781
             
-        # timef1 =  time.time()
-        # ret, frame = cap.read()
-        frame = np.full((10,10, 3), [255, 255, 255], dtype=np.uint8)
-        # timef2 =  time.time()
-        # print("how long to read frame?", timef2 - timef1)# first frame takes a while and subsequent frames are fast: 0.9233419895172119 -> 0.006009101867675781
-        
-        # Recolor Feed
-        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        image.flags.writeable = False        
-        
-        time_1 = time.time()
-        # Make Detections
-        results = holistic.process(image)
-        time_2 = time.time()
+            # Recolor Feed
+            image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            image.flags.writeable = False        
+            
+            time_1 = time.time()
+            # Make Detections
+            results = holistic.process(image)
+            time_2 = time.time()
 
-        # Recolor image back to BGR for rendering
-        image.flags.writeable = True   
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-        
-        # 2. Right hand
-        mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS, 
-                                mp_drawing.DrawingSpec(color=(80,22,10), thickness=2, circle_radius=4),
-                                mp_drawing.DrawingSpec(color=(80,44,121), thickness=2, circle_radius=2)
-                                )
+            # Recolor image back to BGR for rendering
+            image.flags.writeable = True   
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+            
+            # 2. Right hand
+            mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS, 
+                                    mp_drawing.DrawingSpec(color=(80,22,10), thickness=2, circle_radius=4),
+                                    mp_drawing.DrawingSpec(color=(80,44,121), thickness=2, circle_radius=2)
+                                    )
 
-        # 3. Left Hand
-        mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS, 
-                                mp_drawing.DrawingSpec(color=(121,22,76), thickness=2, circle_radius=4),
-                                mp_drawing.DrawingSpec(color=(121,44,250), thickness=2, circle_radius=2)
-                                )
+            # 3. Left Hand
+            mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS, 
+                                    mp_drawing.DrawingSpec(color=(121,22,76), thickness=2, circle_radius=4),
+                                    mp_drawing.DrawingSpec(color=(121,44,250), thickness=2, circle_radius=2)
+                                    )
 
-        # 4. Pose Detections
-        mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS, 
-                                mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=4),
-                                mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2)
-                                )
-                        
-        print("array", cv2.flip(image,0))
+            # 4. Pose Detections
+            mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS, 
+                                    mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=4),
+                                    mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2)
+                                    )
+                            
+            # print("array", cv2.flip(image,0))
+            print("array returns here")
