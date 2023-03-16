81759ea94f36e5e592625c0f87540b147f6594fd
ShootingStarDragon <rppapamaths@gmail.com>
2023-03-16 01:02:58 -0700

    finally merged to just 1 flag, kivy_run_state that tells the subprocesses to exit instead of run_state and fullSTOP flags

--- importFCVA.py
+++ importFCVA.py
@@ -27,51 +27,49 @@ def open_mediapipe(*args):
             # while shared_metadata_dict["run_state"] and cap.isOpened():
             # print("keys?1??", shared_metadata_dict.keys(), flush = True)
             while True:
-                if "fullSTOP" in shared_metadata_dict.keys(): 
-                    print("open_appliedcv breaking!", flush=True)
-                    break
-                else:
-                    frame = shared_metadata_dict["latest_cap_frame"]
-                    # timef2 =  time.time()
-                    # print("how long to read frame?", timef2 - timef1)# first frame takes a while and subsequent frames are fast: 0.9233419895172119 -> 0.006009101867675781
-                    
-                    # Recolor Feed
-                    image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-                    image.flags.writeable = False #I have read that writable false/true this makes things faster for mediapipe holistic
-                    
-                    # Make Detections
-                    results = holistic.process(image)
-                    
-                    # Recolor image back to BGR for rendering
-                    image.flags.writeable = True   
-                    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-                    
-                    # 2. Right hand
-                    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS, 
-                                            mp_drawing.DrawingSpec(color=(80,22,10), thickness=2, circle_radius=4),
-                                            mp_drawing.DrawingSpec(color=(80,44,121), thickness=2, circle_radius=2)
-                                            )
+                if "kivy_run_state" in shared_metadata_dict.keys(): 
+                    if shared_metadata_dict["kivy_run_state"] == False:
+                        break
+            
+                frame = shared_metadata_dict["latest_cap_frame"]
+                # timef2 =  time.time()
+                # print("how long to read frame?", timef2 - timef1)# first frame takes a while and subsequent frames are fast: 0.9233419895172119 -> 0.006009101867675781
+                
+                # Recolor Feed
+                image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                image.flags.writeable = False #I have read that writable false/true this makes things faster for mediapipe holistic
+                
+                # Make Detections
+                results = holistic.process(image)
+                
+                # Recolor image back to BGR for rendering
+                image.flags.writeable = True   
+                image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+                
+                # 2. Right hand
+                mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS, 
+                                        mp_drawing.DrawingSpec(color=(80,22,10), thickness=2, circle_radius=4),
+                                        mp_drawing.DrawingSpec(color=(80,44,121), thickness=2, circle_radius=2)
+                                        )
 
-                    # 3. Left Hand
-                    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS, 
-                                            mp_drawing.DrawingSpec(color=(121,22,76), thickness=2, circle_radius=4),
-                                            mp_drawing.DrawingSpec(color=(121,44,250), thickness=2, circle_radius=2)
-                                            )
+                # 3. Left Hand
+                mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS, 
+                                        mp_drawing.DrawingSpec(color=(121,22,76), thickness=2, circle_radius=4),
+                                        mp_drawing.DrawingSpec(color=(121,44,250), thickness=2, circle_radius=2)
+                                        )
 
-                    # 4. Pose Detections6
-                    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS, 
-                                            mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=4),
-                                            mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2)
-                                            )
-                                    
-                    shared_analysis_dict[1] = cv2.flip(image,0)
+                # 4. Pose Detections6
+                mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS, 
+                                        mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=4),
+                                        mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2)
+                                        )
+                                
+                shared_analysis_dict[1] = cv2.flip(image,0)
     except Exception as e:
         print("open_mediapipe died!", e)
 
 app.appliedcv = open_mediapipe
 
 if __name__ == '__main__' :
-    print("OG MAIN #this is the guy that's not ending : - )", flush =True)
     app.run()
-    print("MAIN ENDED", flush = True)
     
