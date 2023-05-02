7ae5cb563af986d1d3bda299b8a35c71fe2be49a
ShootingStarDragon <rppapamaths@gmail.com>
2023-05-02 08:36:57 -0700

    opencv wins...

--- FastCVApp/examples/creativecommonsmedia/ffmpegprocess.py
+++ FastCVApp/examples/creativecommonsmedia/ffmpegprocess.py
@@ -7,10 +7,11 @@ import numpy as np
 
 in_filename = "Elephants Dream charstart2.webm" 
 
-in_file = open(in_filename, "r") # opening for [r]eading as [b]inary
-inputbytes = in_file.read() 
-in_file.seek(0) #wtf, remember to seek to beginning??
-in_file.close()
+#wow, this is unnecessary
+# in_file = open(in_filename, "r") # opening for [r]eading as [b]inary
+# inputbytes = in_file.read() 
+# in_file.seek(0) #wtf, remember to seek to beginning??
+# in_file.close()
 
 process1 = (
     ffmpeg
@@ -20,7 +21,7 @@ process1 = (
     .run_async(pipe_stdout=True)
 )
 
-process1a = (
+process2 = (
     ffmpeg
     .input(in_filename)
     # .input('pipe:')
@@ -28,7 +29,7 @@ process1a = (
     .run_async(pipe_stdout=True)
 )
 
-process1b = (
+process3 = (
     ffmpeg
     .input(in_filename)
     # .input('pipe:')
@@ -38,6 +39,12 @@ process1b = (
 
 width, height = 1920, 1080
 
+import time
+starttime = time.time()
+guy1 = cv2.VideoCapture(in_filename)
+fps = guy1.get(cv2.CAP_PROP_FPS)
+internal_i = 0
+
 while True:
     # process1.communicate(input=inputbytes)[0]
     # The whole purpose of the communicate method is to wait for the process to finish and return all the output
@@ -45,11 +52,14 @@ while True:
 
     # https://stackoverflow.com/questions/8475290/how-do-i-write-to-a-python-subprocess-stdin
 
-    intoffmpeg = process1.stdin.write(inputbytes[:width * height * 3])
+    # intoffmpeg = process1.stdin.write(inputbytes[:width * height * 3])
 
     in_bytes = process1.stdout.read(width * height * 3)
+    in_bytes2 = process2.stdout.read(width * height * 3)
+    in_bytes3 = process3.stdout.read(width * height * 3)
     if not in_bytes:
         break
+
     in_frame = (
         np
         .frombuffer(in_bytes, np.uint8)
@@ -59,9 +69,34 @@ while True:
     # https://www.scivision.dev/numpy-image-bgr-to-rgb/
     in_frame = in_frame[...,[2, 1, 0]].copy() # this is correct
     cv2.imshow('img', in_frame)  # Show the image for testing
+
+    in_frame2 = (
+        np
+        .frombuffer(in_bytes2, np.uint8)
+        .reshape([height, width, 3])
+    )
+    # out_frame = in_frame * 0.3
+    # https://www.scivision.dev/numpy-image-bgr-to-rgb/
+    in_frame2 = in_frame2[...,[2, 1, 0]].copy() # this is correct
+    cv2.imshow('img2', in_frame)  # Show the image for testing
+
+    in_frame3 = (
+        np
+        .frombuffer(in_bytes3, np.uint8)
+        .reshape([height, width, 3])
+    )
+    # out_frame = in_frame * 0.3
+    # https://www.scivision.dev/numpy-image-bgr-to-rgb/
+    in_frame3 = in_frame3[...,[2, 1, 0]].copy() # this is correct
+    cv2.imshow('img3', in_frame3)  # Show the image for testing
+    
     # cv2.waitKey(1000)
     if cv2.waitKey(25) & 0xFF == ord('q'):
         break
+
+    currentREALframe = int((time.time() - starttime)/(1/fps))
+    print("what is current frame vs actualframe?", internal_i, currentREALframe )
+    internal_i += 1
 
 cv2.destroyAllWindows()
 
