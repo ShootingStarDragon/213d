9ebc3b76a6c58bccd2da1a2286b0661d48345fcd
ShootingStarDragon <rppapamaths@gmail.com>
2023-05-02 01:09:54 -0700

    ValueError: cannot reshape array of size 5922568 into shape (1080,1920,3)

--- FastCVApp/examples/creativecommonsmedia/ffmpegnumpyarray.py
+++ FastCVApp/examples/creativecommonsmedia/ffmpegnumpyarray.py
@@ -3,14 +3,16 @@
 import numpy as np
 import cv2
 import subprocess as sp
-import shlex
+# import shlex
+import ffmpeg
 
 # Build synthetic 1fps video (with a frame counter):
 # Set GOP size to 20 frames (place key frame every 20 frames - for testing).
 #########################################################################
 # W, H = 320, 240 # video dimensions
 W, H = 1920, 1080 # video dimensions
-video_path = "Elephants Dream charstart2FULL.webm"  # path to video
+# video_path = "Elephants Dream charstart2FULL.webm"  # path to video
+video_path = "Elephants Dream charstart2.webm"  # path to video
 # sp.run(shlex.split(f'ffmpeg -y -f lavfi -i testsrc=size={W}x{H}:rate=1 -vcodec libx264 -g 20 -crf 17 -pix_fmt yuv420p -t 60 {video_path}'))
 #########################################################################
 
@@ -19,18 +21,20 @@ in_file.seek(0) #wtf, remember to seek to beginning??
 inputbytes = in_file.read() 
 in_file.close()
 
+#HUGE EXAMPLE, capture_stdout = TRUE, https://stackoverflow.com/questions/75224177/ffmpeg-changes-pixel-values-when-reading-and-saving-png-without-modification
+
 # ffmpeg command
-command = [ 'ffmpeg',
-            '-ss', '00:00:0',    # Seek to 11'th second.
-            '-i', 'pipe:',       #'-i', video_path,
-            '-pix_fmt', 'bgr24',  # brg24 for matching OpenCV
-            '-f', 'rawvideo',
-            '-t', '5',            # Play 5 seconds long
-            'pipe:' ]
+# command = [ 'ffmpeg',
+#             '-ss', '00:00:0',    # Seek to 11'th second.
+#             '-i', 'pipe:',       #'-i', video_path,
+#             '-pix_fmt', 'bgr24',  # brg24 for matching OpenCV
+#             '-f', 'rawvideo',
+#             '-t', '5',            # Play 5 seconds long
+#             'pipe:' ]
 
 # Execute FFmpeg as sub-process with stdout as a pipe
 
-with sp.Popen(command, stdin=sp.PIPE, stdout=sp.PIPE, bufsize=10**8) as process:
+'''with sp.Popen(command, stdin=sp.PIPE, stdout=sp.PIPE, bufsize=10**8) as process:
     output_data = process.communicate(input=inputbytes)[0]
     # https://stackoverflow.com/questions/67877611/passing-bytes-to-ffmpeg-in-python-with-io
     # process.wait()
@@ -63,10 +67,18 @@ with sp.Popen(command, stdin=sp.PIPE, stdout=sp.PIPE, bufsize=10**8) as process:
 
 process.stdout.close()
 process.wait()
-cv2.destroyAllWindows()
-
-'''# Execute FFmpeg as sub-process with stdout as a pipe
-process = sp.Popen(command, stdin=sp.PIPE, stdout=sp.PIPE, bufsize=10**8)
+cv2.destroyAllWindows()'''
+args = (ffmpeg
+    .input('pipe:')
+    # ... extra processing here "-f mkv", example command here:       ffmpeg -i test.wav -f avi pipe: | cat > test.avi        https://ffmpeg.org/ffmpeg-protocols.html#pipe
+    # Special option names: from https://github.com/kkroening/ffmpeg-python
+    # .output('pipe:', **{'format': 'avi'}) #WORKS BUT TAKES A LONG TIME
+    .output('pipe:',pix_fmt='bgr24', **{'format': 'matroska'}) # mkv should be matroska: https://superuser.com/a/846508
+    # .run_async(pipe_stdout=True, pipe_stderr=True)
+    .get_args())
+process = sp.Popen(['ffmpeg'] + args, stdin=sp.PIPE, stdout=sp.PIPE)
+# Execute FFmpeg as sub-process with stdout as a pipe
+# process = sp.Popen(command, stdin=sp.PIPE, stdout=sp.PIPE, bufsize=10**8)
 output_data = process.communicate(input=inputbytes)[0]
 # https://stackoverflow.com/questions/67877611/passing-bytes-to-ffmpeg-in-python-with-io
 process.wait()
@@ -82,14 +94,18 @@ print("STOPPPP")
 while True:
     print("GOOOO")
     # Read decoded video frame (in raw video format) from stdout process.
-    buffer = process.stdout.read(W*H*3)
+    # buffer = process.stdout.read(W*H*3)
+    # buffer = output_data.read(W*H*3)
+    # buffer = output_data[:W*H*3]
+    buffer = output_data
 
     # Break the loop if buffer length is not W*H*3 (when FFmpeg streaming ends).
-    if len(buffer) != W*H*3:
-        break
+    # if len(buffer) != W*H*3:
+    #     break
 
     img = np.frombuffer(buffer, np.uint8).reshape(H, W, 3)
 
+    print("where imshow?")
     cv2.imshow('img', img)  # Show the image for testing
     # cv2.waitKey(1000)
     if cv2.waitKey(25) & 0xFF == ord('q'):
@@ -97,7 +113,7 @@ while True:
 
 process.stdout.close()
 process.wait()
-cv2.destroyAllWindows()'''
+cv2.destroyAllWindows()
 
 
 # # 6000 frames:
