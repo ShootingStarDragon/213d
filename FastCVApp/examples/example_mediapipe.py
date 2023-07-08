8fe30ca9728004a3e82fa9123b819f97405ca679
ShootingStarDragon <rppapamaths@gmail.com>
2023-07-08 15:23:51 -0700

    test cred

--- FastCVApp/examples/example_mediapipe.py
+++ FastCVApp/examples/example_mediapipe.py
@@ -62,6 +62,7 @@ def draw_landmarks_on_image(annotated_image, detection_result):
         import traceback
         print("full exception", "".join(traceback.format_exception(*sys.exc_info())))
 
+
 def apply_mediapipe_func(*args): #basicmp
     try:
         inputdeque = args[0]
